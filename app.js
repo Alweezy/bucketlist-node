c1c2f1d99b5d05c644fec871ee0866cf84d2e9d5
Alweezy <alvinmutisya@gmail.com>
2017-12-11 21:21:21 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+//initialize app variable
+const app = express();
+
+//port declaration
+const port = 3000;
